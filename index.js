df5825695a08ded04201311971219c6b4ead9955
Carolin Ivens <carolin.ivens@gmail.com>
2018-12-21 01:23:16 +0100

    added different reducers

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // Library Code
 function createStore (reducer) {
   // The store should have four parts
@@ -27,18 +26,38 @@ function createStore (reducer) {
   return {
     getState,
     subscribe,
-    dispatch
+    dispatch,
   }
 }
 
+// App Code
 function todos (state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+  switch(action.type) {
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO' :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default :
+      return state
   }
-  return state
+}
+
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
 }
 
 const store = createStore(todos)
+
 store.subscribe(() => {
   console.log('The new state is: ', store.getState())
 })
